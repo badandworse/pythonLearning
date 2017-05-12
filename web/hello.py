d4463da50f12f07296230f53272f431641a5d8bb
bad <zilaixv@gmail.com>
2017-05-12 21:55:32 +0800

    ''save change

--- web/hello.py
+++ web/hello.py
@@ -2,5 +2,5 @@
 
 def application(environ,start_respose):
     start_respose('200 OK',[('Content-Type','text/html')])
-    body='<h1>Hello,%s!</h1>' %(environ['PATH_INFO'][1:])
+    body='<h1>Hello,%s!</h1>' %(environ['PATH_INFO'][1:] or web)
     return [b'<h1>hello,web!</h1>']
