d721ceceb36ff7452dbcbb81dc36ef01aa44835a
bad <zilaixv@gmail.com>
2017-05-13 15:51:58 +0800

    mvc Test

--- web/app.py
+++ web/app.py
@@ -1,29 +1,27 @@
 from flask import Flask
 from flask import request
+from flask import render_template
 
 app=Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def home():
-    return '<h1>Home</h1>'
+    return render_template('home.html')
 
 
 @app.route('/signin',methods=['GET'])
 def signin_form():
-    return '''<form action="/signin" method="post">
-              <p><input name="username"></p>
-              <p><input name="password" type="password"></p>
-              <p><button type="submit">Sign In</button></p>
-              </form>'''
+    return render_template('form.html')
 
 
 @app.route('/signin',methods=['POST'])
 def signin():
     #需要从request对象读取表单内容:
-    if request.form['username']=='admin' and request.form['password']=='12345':
-        return '<h3>hello,admin!</h3>'
-    return '<h3>Bad username or password</h3>'
-
+    username=request.form['username']
+    password=request.form['password']
+    if username=='admin' and password=='12345':
+        return render_template('signin-ok.html',username=username)
+    return render_template('form.html',message='Bad usernameor password',username=username)
 
 if __name__=='__main__':
     app.run()
