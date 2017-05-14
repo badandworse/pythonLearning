1af3bf914b5097bae6f19adfee0dc65f8aba7e71
bad <zilaixv@gmail.com>
2017-05-14 10:54:34 +0800

    two coroutine

--- AsynchronousLearning/asyncio_hello.py
+++ AsynchronousLearning/asyncio_hello.py
@@ -1,15 +1,17 @@
 import asyncio
+import threading
 
 @asyncio.coroutine
 def hello():
-    print("Hello world")
+    print("Hello world (%s)" %threading.current_thread())
     #异步调用asyncio.sleep(1)
-    r=yield from asyncio.sleep(10)
-    print("Hello again")
+    r=yield from asyncio.sleep(3)
+    print("Hello again (%s)" %threading.currentThread())
 
 
 #获取EventLoop
 loop=asyncio.get_event_loop()
+tasks=[hello(),hello()]
 #执行coroutine
-loop.run_until_complete(hello())
+loop.run_until_complete(asyncio.wait(tasks))
 loop.close()
